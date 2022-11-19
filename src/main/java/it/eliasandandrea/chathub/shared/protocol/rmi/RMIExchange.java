88ddad44646cfb3be611e38ef64a4dde1cd47445
Elias Binder <ebinder@unibz.it>
2022-11-19 16:03:08 +0100

    Update RMIExchange.java

--- src/main/java/it/eliasandandrea/chathub/shared/protocol/rmi/RMIExchange.java
+++ src/main/java/it/eliasandandrea/chathub/shared/protocol/rmi/RMIExchange.java
@@ -8,6 +8,6 @@ public interface RMIExchange extends Remote {
 
     //response: ServerEvent
     //parameter: ClientEvent
-    Packet sendMessage(Packet evt);
+    Packet sendMessage(Packet evt, String senderUUID);
 
 }

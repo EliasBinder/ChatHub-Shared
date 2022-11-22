a7c0093741f6aa909dabb7e16d54f840245103ba
Elias Binder <ebinder@unibz.it>
2022-11-23 00:09:44 +0100

    Fix MessageCallbackEvent

--- src/main/java/it/eliasandandrea/chathub/shared/protocol/rmi/MessageCallbackEvent.java
+++ src/main/java/it/eliasandandrea/chathub/shared/protocol/rmi/MessageCallbackEvent.java
@@ -3,9 +3,9 @@ package it.eliasandandrea.chathub.shared.protocol.rmi;
 import it.eliasandandrea.chathub.shared.crypto.EncryptedObjectPacket;
 import it.eliasandandrea.chathub.shared.protocol.ServerEvent;
 
-import java.rmi.Remote;
+import java.io.Serializable;
 
-public interface MessageCallbackEvent extends Remote {
+public interface MessageCallbackEvent extends Serializable {
 
     //parameter: ServerEvent
     void sendAsyncMessage(EncryptedObjectPacket event);
